df8590ca6f2edcc9a12cd5fa97f1e30737569057
Ricardo Araujo <ricarrrdoluigi@gmail.com>
2019-04-28 02:29:44 -0300

    Adding Promise

--- Challenge/app/requests/Request.js
+++ Challenge/app/requests/Request.js
@@ -8,9 +8,22 @@ export default class Request {
 
     request(endpoint){
         const url = host + endpoint
-        axios.get(url)
+
+        return new Promise((resolve, reject) => {
+            axios.get(url)
             .then((response) => {
                 console.log(response.data);
+                if (typeof response.data.success !== 'undefined') {
+                    if (response.data.success) {
+                      return resolve(response.data);
+                    }
+        
+                    return reject(response.data);
+                  }
+        
+                  return resolve(response.data);
             })
+        })
+        
     }
 }
